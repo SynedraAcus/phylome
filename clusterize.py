434b886f6e04de64ad57103e9787ff3ecd3971c2
synedraacus <alexeymorozov1991@gmail.com>
2017-02-02 12:21:47 +0800

    Correctly accepting arguments

--- clusterize.py
+++ clusterize.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import sys
 import collections.abc
 from argparse import ArgumentParser
 from Bio import SeqIO
@@ -19,6 +20,12 @@ class Cluster(collections.abc.MutableSet):
     A cluster of Sequence objects.
     """
     def __init__(self, elements, cutoff=0.3):
+        """
+        Create a new cluster with some elements.
+        Does not check distances between elements on Cluster creation.
+        :param elements: An iterable of elements
+        :param cutoff: A maximum possible distance between elements
+        """
         assert all(isinstance(x, Element) for x in elements)
         self.sequences = set(elements)
         self.cutoff = cutoff
@@ -35,13 +42,12 @@ class Cluster(collections.abc.MutableSet):
 
     def should_accept(self, item):
         """
-        Return True if the item is similar enough to all items in cluster
+        Return True if the item is similar enough to all items in cluster.
         :param item:
         :return:
         """
-        for a in self:
-            # No problem if it walks through this zero times
-            if ffp_distance(a.composition, item.composition) > self.cutoff:
+        for x in self:
+            if ffp_distance(x.composition, item.composition) > self.cutoff:
                 return False
         return True
 
@@ -56,7 +62,7 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='A fast k-mer based clusterization tool')
     parser.add_argument('-f', type=str, nargs='*', help='FASTA file(s)')
     parser.add_argument('-t', type=float, default=0.3,
-                        help='Clustering distance cutoff')
+                        help='Clustering distance cutoff. Default 0.3')
     args = parser.parse_args()
 
     clusters = []
@@ -68,10 +74,10 @@ if __name__ == '__main__':
                 if cluster.should_accept(element):
                     cluster.add(element)
                     accepted = True
+                    break  # Don't accept to several clusters
             if not accepted:
-                clusters.append(Cluster((element, )))
-                print('.')
+                clusters.append(Cluster((element, ), cutoff=args.t))
     print('Built {0} clusters'.format(len(clusters)))
     for cluster in clusters:
-        print(' '.join((x.id for x in cluster)))
+        print(', '.join((x.id for x in cluster)))
 
