44d66ce95aab8890cffcbf5c0c2b771a14965543
synedraacus <alexeymorozov1991@gmail.com>
2017-03-21 12:53:23 +0800

    Some unittests

--- phylomics.py
+++ phylomics.py
@@ -9,6 +9,7 @@ from io import TextIOBase
 BlastHit = namedtuple('BlastHit', ['query_id', 'hit_id', 'query_pos', 'hit_pos',
                                    'evalue'])
 
+
 def parse_blast_line(line):
     """
     Take a single line from BLAST file and return a BlastHit.
@@ -20,6 +21,8 @@ def parse_blast_line(line):
     """
     line = line.rstrip('\n')
     arr = line.split('\t')
+    if not len(arr) == 12:
+        raise ValueError('Incorrect BLAST line {}'.format(line))
     #  There is no explicit validity check, but hopefully IndexError or some
     #  exception of the type converters will be raised on the invalid string
     return BlastHit(query_id=arr[0],
@@ -27,6 +30,7 @@ def parse_blast_line(line):
                     query_pos=(int(arr[6]), int(arr[7])),
                     hit_pos=(int(arr[8]), int(arr[9])),
                     evalue=float(arr[10]))
+
 
 def parse_blast_file(file):
     """
@@ -46,6 +50,6 @@ def parse_blast_file(file):
     else:
         raise TypeError('Only string or readable text-mode filehandle accepted by parse_blast_file')
     for line in handle:
-        if not line[0] == '#'
+        if not line[0] == '#':
             yield parse_blast_line(line)
     # Not closing a filehandle because it can be used by the calling code.
--- test_phylomics.py
+++ test_phylomics.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
 #  Unit tests for phylomics.py
+import pytest
 from phylomics import BlastHit, parse_blast_line, parse_blast_file
 
-def test_blast_lines():
+def test_valid_blast_lines():
     # Parsing valid line
     assert parse_blast_line('Achnanthes_exigua_ABQ43357.1	NODE_3018_length_2386_cov_4738.01	76.37	182	43	0	1	182	411	956	5e-88	 277') ==\
         BlastHit(query_id='Achnanthes_exigua_ABQ43357.1',
@@ -11,4 +12,47 @@ def test_blast_lines():
                  query_pos=(1, 182),
                  hit_pos=(411, 956),
                  evalue=float('5e-88'))
+    # Numeric IDs?
+    assert parse_blast_line('1	2	76.37	182	43	0	1	182	411	956	5e-88	 277') == \
+       BlastHit(query_id='1',
+                hit_id='2',
+                query_pos=(1, 182),
+                hit_pos=(411, 956),
+                evalue=float('5e-88'))
 
+
+def test_invalid_blast_lines():
+    # A single field missing
+    with pytest.raises(ValueError):
+        parse_blast_line('1	2	76.37	182	43	0	1	411	956	5e-88	 277')
+    # One field too much
+    with pytest.raises(ValueError):
+        parse_blast_line('0\t1	2	76.37	182	43	0	1	182	411	956	5e-88	 277')
+    with pytest.raises(ValueError):
+        parse_blast_line('')
+
+
+def test_valid_blast_file():
+    #  Check that the generator returns 50 BlastHit`s without raising anything
+    # A filename passed as a string
+    l = list(parse_blast_file('test_data/BLAST_test.tsv'))
+    assert len(l) == 50 and all(isinstance(x, BlastHit) for x in l)
+    # A filename passed as a filehandle
+    l = list(parse_blast_file(open('test_data/BLAST_test.tsv')))
+    assert len(l) == 50 and all(isinstance(x, BlastHit) for x in l)
+
+
+def test_invalid_blast_file():
+    with pytest.raises(TypeError):
+        g = parse_blast_file(42)
+    with pytest.raises(TypeError):
+        g = parse_blast_file([1, 2, 'qwerty'])
+    # File doesn't exist:
+    with pytest.raises(FileNotFoundError):
+        g = parse_blast_file('asdf')
+    # A file exists, but isn't BLAST
+    with pytest.raises(ValueError):
+        g = parse_blast_file('README.md')
+    # Same for handle
+    with pytest.raises(ValueError):
+        g = parse_blast_file(open('README.md', mode='r'))
