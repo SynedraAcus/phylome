ba64550613b7b246d5d6d16fe1d2428cd79c73b2
synedraacus <alexeymorozov1991@gmail.com>
2017-03-22 16:14:01 +0800

    Hit namedtuple and hit generator

--- phylomics.py
+++ phylomics.py
@@ -6,18 +6,19 @@ from collections import namedtuple
 from io import TextIOBase
 
 # Lightweight BLAST table parser
-BlastHit = namedtuple('BlastHit', ['query_id', 'hit_id', 'query_pos', 'hit_pos',
+BlastHSP = namedtuple('BlastHSP', ['query_id', 'hit_id', 'query_pos', 'hit_pos',
                                    'evalue'])
+BlastHit = namedtuple('BlastHit', ['query_id', 'hit_id', 'hsps'])
 
 
 def parse_blast_line(line):
     """
-    Take a single line from BLAST file and return a BlastHit.
+    Take a single line from BLAST file and return a BlastHSP.
     It assumes the default tabular output from BLAST 2.2.28+ (generated with
     `-outfmt 6` without any additional format specifications). May or may not
     work with other BLAST versions.
     :param line: str
-    :return: BlastHit
+    :return: BlastHSP
     """
     line = line.rstrip('\n')
     arr = line.split('\t')
@@ -25,16 +26,16 @@ def parse_blast_line(line):
         raise ValueError('Incorrect BLAST line {}'.format(line))
     #  There is no explicit validity check, but hopefully IndexError or some
     #  exception of the type converters will be raised on the invalid string
-    return BlastHit(query_id=arr[0],
+    return BlastHSP(query_id=arr[0],
                     hit_id=arr[1],
                     query_pos=(int(arr[6]), int(arr[7])),
                     hit_pos=(int(arr[8]), int(arr[9])),
                     evalue=float(arr[10]))
 
 
-def parse_blast_file(filename):
+def parse_blast_file_to_hsps(filename):
     """
-    Take a BLAST output file and generate BlastHit instances from its lines.
+    Take a BLAST output file and generate BlastHSP instances from its lines.
     It assumes the default tabular output from BLAST 2.2.28+ (generated with
     `outfmt 6` or `outfmt 7` without any additional format specifications).
     Any line starting with `#` is considered a comment and omitted.
@@ -53,3 +54,28 @@ def parse_blast_file(filename):
         if not line[0] == '#':
             yield parse_blast_line(line)
     # Not closing a filehandle because it can be used by the calling code.
+
+
+#  Assemble hits
+def assemble_hits(iterable):
+    """
+    Groups HSPs into BlastHit instances
+    Takes an iterable of BlastHSPs, yields BlastHits
+    :param iterable:
+    :return:
+    """
+    y = []
+    current_hit = ''
+    current_query = ''
+    for blast_hsp in iterable:
+        #  The following two checks will only run when the generator is created
+        if not current_hit:
+            current_hit = blast_hsp.hit_id
+        if not current_query:
+            current_query = blast_hsp.query_id
+        y.append(blast_hsp)
+        if current_hit !=blast_hsp.hit_id or current_query != blast_hsp.query_id:
+            yield BlastHit(query_id=current_query, hit_id=current_hit, hsps=y)
+            y = []
+            current_hit = blast_hsp.hit_id
+            current_query = blast_hsp.query_id
--- test_phylomics.py
+++ test_phylomics.py
@@ -2,24 +2,25 @@
 
 #  Unit tests for phylomics.py
 import pytest
-from phylomics import BlastHit, parse_blast_line, parse_blast_file
+from phylomics import BlastHSP, parse_blast_line, parse_blast_file_to_hsps, \
+    assemble_hits, BlastHit
 
 
 def test_valid_blast_lines():
     # Parsing valid line
-    assert parse_blast_line('Achnanthes_exigua_ABQ43357.1	NODE_3018_length_2386_cov_4738.01	76.37	182	43	0	1	182	411	956	5e-88	 277') ==\
-        BlastHit(query_id='Achnanthes_exigua_ABQ43357.1',
-                 hit_id='NODE_3018_length_2386_cov_4738.01',
-                 query_pos=(1, 182),
-                 hit_pos=(411, 956),
-                 evalue=float('5e-88'))
+    assert parse_blast_line('Achnanthes_exigua_ABQ43357.1	NODE_3018_length_2386_cov_4738.01	76.37	182	43	0	1	182	411	956	5e-88	 277') == \
+           BlastHSP(query_id='Achnanthes_exigua_ABQ43357.1',
+                    hit_id='NODE_3018_length_2386_cov_4738.01',
+                    query_pos=(1, 182),
+                    hit_pos=(411, 956),
+                    evalue=float('5e-88'))
     # Numeric IDs?
     assert parse_blast_line('1	2	76.37	182	43	0	1	182	411	956	5e-88	 277') == \
-       BlastHit(query_id='1',
-                hit_id='2',
-                query_pos=(1, 182),
-                hit_pos=(411, 956),
-                evalue=float('5e-88'))
+           BlastHSP(query_id='1',
+                    hit_id='2',
+                    query_pos=(1, 182),
+                    hit_pos=(411, 956),
+                    evalue=float('5e-88'))
 
 
 def test_invalid_blast_lines():
@@ -39,26 +40,34 @@ def test_invalid_blast_lines():
 
 
 def test_valid_blast_file():
-    #  Check that the generator returns 50 BlastHit`s without raising anything
+    #  Check that the generator returns 50 BlastHSP`s without raising anything
     # A filename passed as a string
-    l = list(parse_blast_file('test_data/BLAST_test.tsv'))
-    assert len(l) == 50 and all(isinstance(x, BlastHit) for x in l)
+    l = list(parse_blast_file_to_hsps('test_data/BLAST_test.tsv'))
+    assert len(l) == 50 and all(isinstance(x, BlastHSP) for x in l)
     # A filename passed as a filehandle
-    l = list(parse_blast_file(open('test_data/BLAST_test.tsv')))
-    assert len(l) == 50 and all(isinstance(x, BlastHit) for x in l)
+    l = list(parse_blast_file_to_hsps(open('test_data/BLAST_test.tsv')))
+    assert len(l) == 50 and all(isinstance(x, BlastHSP) for x in l)
 
 
 def test_invalid_blast_file():
     with pytest.raises(TypeError):
-        g = list(parse_blast_file(42))
+        g = list(parse_blast_file_to_hsps(42))
     with pytest.raises(TypeError):
-        g = list(parse_blast_file([1, 2, 'qwerty']))
+        g = list(parse_blast_file_to_hsps([1, 2, 'qwerty']))
     # File doesn't exist:
     with pytest.raises(FileNotFoundError):
-        g = list(parse_blast_file('asdf'))
+        g = list(parse_blast_file_to_hsps('asdf'))
     # A file exists, but isn't BLAST
     with pytest.raises(ValueError):
-        g = list(parse_blast_file('README.md'))
+        g = list(parse_blast_file_to_hsps('README.md'))
     # Same for handle
     with pytest.raises(ValueError):
-        g = list(parse_blast_file(open('README.md', mode='r')))
+        g = list(parse_blast_file_to_hsps(open('README.md', mode='r')))
+
+
+def test_assemble_hits():
+    # This file contains 46 hits, only 4 out of which have 2 hsps
+    hits = list(assemble_hits(parse_blast_file_to_hsps('test_data/BLAST_test.tsv')))
+    assert len(hits) == 45
+    assert all(isinstance(x, BlastHit) for x in hits)
+    assert len([x for x in hits if len(x.hsps) == 2]) == 5
