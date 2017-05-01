a14f8e22d317335762940e16c0c90ec974258cf2
SynedraAcus <alexeymorozov1991@gmail.com>
2017-05-01 13:36:25 +0800

    Minor fixes

--- find_multiplicates.py
+++ find_multiplicates.py
@@ -29,7 +29,8 @@ parser.add_argument('-f', type=str, default='',
                     help='FASTA file of queries')
 parser.add_argument('-o', type=float, help='Overlap cutoff. Default 0.5')
 parser.add_argument('-l', type=int, help='Length cutoff. Default 50')
-parser.add_argument('-c', type=float, help=)
+parser.add_argument('-c', type=float, default=0.5,
+                    help='Duplicate hit percentage. Default 0.5')
 parser.add_argument('-n', type=str, default='multiples',
                     help='Output filename')
 args = parser.parse_args()
@@ -41,13 +42,14 @@ if not args.b:
 duplicates = set()
 for query_group in iterate_by_query(parse_blast_file_to_hits(filename=args.b)):
     if duplicate_percentage(query_group, overlap_cutoff=args.o,
-                            len_cutoff=args.l):
+                            len_cutoff=args.l) >= args.c:
         duplicates.add(query_group[0].query_id)
-
+print('Found {} duplicates'.format(len(duplicates)), file=stderr)
 if duplicates:
     with open('{}.duplicate_list'.format(args.n), mode='w') as duplicate_list:
         for a in duplicates:
             print(a, file=duplicate_list)
+    print('Generating FASTA...', file=stderr)
     if args.f:
         with open('{}.duplicates.fasta'.format(args.n), mode='w') as outfasta:
             for record in SeqIO.parse(args.f, 'fasta'):
