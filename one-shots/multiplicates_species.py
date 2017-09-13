d09df97495973bd4d11f042b3f2ebdf3775bebff
synedraacus <alexeymorozov1991@gmail.com>
2017-09-13 15:48:50 +0800

    Added total counts to multispecies

--- one-shots/multiplicates_species.py
+++ one-shots/multiplicates_species.py
@@ -9,13 +9,16 @@ parser.add_argument('-t', type=str, help='Sequence ID to taxon mapping')
 args = parser.parse_args()
 
 species = {}
-species_uniq = set()
+species_totals = {}
 for line in open(args.t):
     a = line.rstrip().split('\t')
     species[a[0]] = a[1]
-    species_uniq.add(a[1])
-counts = {x: 0 for x in species_uniq}
+    if a[1] in species_totals.keys():
+        species_totals[a[1]] += 1
+    else:
+        species_totals[a[1]] = 1
+counts = {x: 0 for x in species_totals.keys()}
 for record in SeqIO.parse(args.f, format='fasta'):
     counts[species[record.id]] += 1
 for a in counts.keys():
-    print('\t'.join((a, str(counts[a]))))
+    print('\t'.join((a, str(counts[a]), str(counts[a]/species_totals[a]))))
