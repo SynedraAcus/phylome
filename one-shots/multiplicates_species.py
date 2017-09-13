6ec11a714df02e006511f9c507d569011427e0ce
synedraacus <alexeymorozov1991@gmail.com>
2017-09-13 14:35:51 +0800

    species counter

---
+++ one-shots/multiplicates_species.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+from argparse import ArgumentParser
+from Bio import SeqIO
+
+parser = ArgumentParser('Count sequences from different species')
+parser.add_argument('-f', type=str, help='Sequence FASTA file')
+parser.add_argument('-t', type=str, help='Sequence ID to taxon mapping')
+args = parser.parse_args()
+
+species = {}
+species_uniq = set()
+for line in open(args.t):
+    a = line.rstrip().split('\t')
+    species[a[0]] = a[1]
+    species_uniq.add(a[1])
+counts = {x: 0 for x in species_uniq}
+for record in SeqIO.parse(args.f, format='fasta'):
+    counts[species[record.id]] += 1
+for a in counts.keys():
+    print('\t'.join((a, counts[a])))
