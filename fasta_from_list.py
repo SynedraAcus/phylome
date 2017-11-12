892d01dc0476499620daeb1d22d3a880fb9790c2
SynedraAcus <alexeymorozov1991@gmail.com>
2017-11-12 16:21:47 +0800

    rewritten fasta_from_list for new clustering

--- fasta_from_list.py
+++ fasta_from_list.py
@@ -1,34 +1,43 @@
 #! /usr/bin/env python3
 
-from Bio import SeqIO
+import os
 from argparse import ArgumentParser
+
+from Bio import SeqIO
 
 parser = ArgumentParser(description='Generate a lot of FASTAs using list file')
 parser.add_argument('-l', type=str, help='List file')
 parser.add_argument('-f', type=str, nargs='+', help='FASTA file(s)')
 parser.add_argument('-o', type=str, help='Output basename. Default `cluster`',
                     default='cluster')
+parser.add_argument('-d', type=str, default='groups',
+                    help='Output directory to be created')
+parser.add_argument('--min', type=int, default=50,
+                    help='Minimal cluster size')
+parser.add_argument('--max', type=int, default=200,
+                    help='Maximal cluster size')
 args = parser.parse_args()
 
 clusters = []
 filehandles = []
 counter = 0
+os.mkdir(args.d)
+# Assumes list to consist of tab-separated ID lists, one cluster in each line
 for line in open(args.l):
-    counter += 1
-    exec('clusters.append({})'.format(line.rstrip()))
-    if not type(clusters[-1]) is set:
-        raise ValueError('Incorrect line {}'.format(line))
-    filehandles.append(open('{0}{1}.fasta'.format(args.o,
-                                                  counter),
-                            mode='w'))
+    ids = line.rstrip().split('\t')
+    if args.min <= len(ids) <= args.max:
+        clusters.append(set(ids))
+        filehandles.append(open('{0}/{1}{2}.fasta'.format(args.d, args.o,
+                                                      counter),
+                           mode='w+'))
+        counter += 1
 
-records = [[] for x in clusters]
 for fasta_file in args.f:
-    for record in SeqIO.parse(fasta_file, 'fasta'):
+    fasta_iterator = SeqIO.parse(fasta_file, 'fasta')
+    pwd = os.getcwd()
+    os.chdir(args.d)
+    for record in fasta_iterator:
         for index, cluster in enumerate(clusters):
             if record.id in cluster:
-                records[index].append(cluster)
-
-for index, record_group in enumerate(records):
-    SeqIO.write(record_group, filehandles[index])
-
+                SeqIO.write(record, filehandles[index], 'fasta')
+    os.chdir(pwd)
