96d100c69d07d76c7a090aa446084365d3c3fec2
SynedraAcus <alexeymorozov1991@gmail.com>
2017-11-12 17:00:47 +0800

    Added filters to fasta_from_list

--- fasta_from_list.py
+++ fasta_from_list.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import os
+import sys
 from argparse import ArgumentParser
 
 from Bio import SeqIO
@@ -12,25 +13,31 @@ parser.add_argument('-o', type=str, help='Output basename. Default `cluster`',
                     default='cluster')
 parser.add_argument('-d', type=str, default='groups',
                     help='Output directory to be created')
-parser.add_argument('--min', type=int, default=50,
+parser.add_argument('--min', type=int, default=30,
                     help='Minimal cluster size')
 parser.add_argument('--max', type=int, default=200,
                     help='Maximal cluster size')
+parser.add_argument('--min_species', type=int, default=10,
+                    help='Minimum species count in cluster')
 args = parser.parse_args()
 
 clusters = []
-filehandles = []
+diatom_filehandles = []
 counter = 0
 os.mkdir(args.d)
 # Assumes list to consist of tab-separated ID lists, one cluster in each line
 for line in open(args.l):
     ids = line.rstrip().split('\t')
-    if args.min <= len(ids) <= args.max:
+    species = len({x.split('|')[0] for x in ids})
+    if args.min <= len(ids) <= args.max and species >= args.min_species:
+        print(species)
         clusters.append(set(ids))
-        filehandles.append(open('{0}/{1}{2}.fasta'.format(args.d, args.o,
-                                                      counter),
-                           mode='w+'))
+        diatom_filehandles.append(open('{0}/{1}{2}.diatoms.fasta'.format(args.d,
+                                                                     args.o,
+                                                                     counter),
+                                       mode='w+'))
         counter += 1
+print('Loaded {} clusters'.format(counter), flush=True, file=sys.stderr)
 
 for fasta_file in args.f:
     fasta_iterator = SeqIO.parse(fasta_file, 'fasta')
@@ -39,5 +46,6 @@ for fasta_file in args.f:
     for record in fasta_iterator:
         for index, cluster in enumerate(clusters):
             if record.id in cluster:
-                SeqIO.write(record, filehandles[index], 'fasta')
+                SeqIO.write(record, diatom_filehandles[index], 'fasta')
     os.chdir(pwd)
+print('Loaded diatom FASTAs', flush=True, file=sys.stderr)
