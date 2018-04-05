62578b5aba64fd9fd91a93f238c941d2ef91ee2f
synedraacus <alexeymorozov1991@gmail.com>
2018-04-05 16:14:51 +0800

    Redirecting output

--- one-shots/generate_iqtree_calls.py
+++ one-shots/generate_iqtree_calls.py
@@ -8,7 +8,7 @@ parser = ArgumentParser('Create IQtree call lists')
 parser.add_argument('-d', type=str, help='Alignment directory')
 args = parser.parse_args()
 
-command_mask = '/home/amorozov/tools/iqtree-1.6.1-Linux/bin/iqtree -s {} -m LG+F+G -st AA -nt 1'
+command_mask = '/home/amorozov/tools/iqtree-1.6.1-Linux/bin/iqtree -s {} -m LG+F+G -st AA -nt 1 >{}.iqtree.stdout 2>{}.iqtree.stderr'
 if not os.path.isdir(args.d):
     raise ValueError('Incorrect path')
 alignments = glob(args.d+'/*.aln.fasta')
