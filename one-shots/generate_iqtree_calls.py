b650849b57d1806a9fa4feffe9002075717bbd81
synedraacus <alexeymorozov1991@gmail.com>
2018-04-05 16:18:10 +0800

    Fixed a bug

--- one-shots/generate_iqtree_calls.py
+++ one-shots/generate_iqtree_calls.py
@@ -8,10 +8,11 @@ parser = ArgumentParser('Create IQtree call lists')
 parser.add_argument('-d', type=str, help='Alignment directory')
 args = parser.parse_args()
 
-command_mask = '/home/amorozov/tools/iqtree-1.6.1-Linux/bin/iqtree -s {} -m LG+F+G -st AA -nt 1 >{}.iqtree.stdout 2>{}.iqtree.stderr'
+command_mask = '/home/amorozov/tools/iqtree-1.6.1-Linux/bin/iqtree -s {0} -m LG+F+G -st AA -nt 1 >{0}.iqtree.stdout 2>{0}.iqtree.stderr'
 if not os.path.isdir(args.d):
     raise ValueError('Incorrect path')
 alignments = glob(args.d+'/*.aln.fasta')
 for alignment in alignments:
+    print(alignment)
     print(command_mask.format(os.path.abspath(alignment)))
 
