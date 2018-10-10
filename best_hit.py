a120142c99bb4022193d1f8eede7f0fe3ff4442a
synedraacus <alexeymorozov1991@gmail.com>
2018-10-10 17:16:04 +0800

    Using only a single best hit

--- best_hit.py
+++ best_hit.py
@@ -13,6 +13,8 @@ parser.add_argument('-d', type=str, help='mySQL DB name')
 parser.add_argument('-o', type=str, help='mySQL host')
 parser.add_argument('-u', type=str, help='mySQL username')
 parser.add_argument('-p', type=str, help='mySQL password')
+parser.add_argument('--best', action='store_true',
+                help='Store only if the Archaeplastida hit is the very best')
 args = parser.parse_args()
 
 cnx = pymysql.connect(user=args.u, host=args.o, password=args.p,
@@ -24,8 +26,8 @@ taxid_request = 'select * from acc2taxid where accession in ({});'
 red_handle = open(args.f + '.reds', mode='w')
 green_handle = open(args.f + 'greens', mode='w')
 none_handle = open(args.f + '.none', mode='w')
+best = {'red': [], 'green': [], 'none': []}
 for hit_list in iterate_by_query(parse_blast_file_to_hits(filename=args.f)):
-    best = {'red': [], 'green': [], 'none': []}
     # Loading the relevant piece of acc2taxid
     r = taxid_request.format(', '.join(['"'+x.hit_id.split('.')[0]+'"' for x in hit_list]))
     cursor.execute(r)
@@ -33,22 +35,38 @@ for hit_list in iterate_by_query(parse_blast_file_to_hits(filename=args.f)):
     acc2taxid = {}
     for x in result:
         acc2taxid[x[0]] = x[1]
-    for hit in sorted(hit_list,
-                      key=lambda x: min([hsp.evalue for hsp in x.hsps])):
+    l = sorted(hit_list, key=lambda x: min([hsp.evalue for hsp in x.hsps]))
+    if args.best:
+        hit = l[0]
         supertaxon = get_supertaxon_from_list(acc2taxid[hit.hit_id.split('.')[0]],
                                               [2166, 33090],
                                               cursor)
         if supertaxon:
-            print(hit.hit_id, supertaxon)
+            print(hit.query_id, supertaxon)
             if supertaxon == 2166:
-                print(hit.hit_id, file=red_handle)
-                best['red'].append(hit.hit_id)
+                print(hit.query_id, file=red_handle)
+                best['red'].append(hit.query_id)
             elif supertaxon == 33090:
-                best['green'].append(hit.hit_id)
-                print(hit.hit_id, file=green_handle)
-            break
+                best['green'].append(hit.query_id)
+                print(hit.query_id, file=green_handle)
         else:
-            print(hit.hit_id, file=none_handle)
+            print(hit.query_id, file=none_handle)
+    else:
+        for hit in l:
+            supertaxon = get_supertaxon_from_list(acc2taxid[hit.hit_id.split('.')[0]],
+                                                  [2166, 33090],
+                                                  cursor)
+            if supertaxon:
+                print(hit.query_id, supertaxon)
+                if supertaxon == 2166:
+                    print(hit.query_id, file=red_handle)
+                    best['red'].append(hit.query_id)
+                elif supertaxon == 33090:
+                    best['green'].append(hit.query_id)
+                    print(hit.query_id, file=green_handle)
+                break
+            else:
+                print(hit.query_id, file=none_handle)
 
 print('Red\t{}\nGreen\t{}\nNone\t{}'.format(len(best['red']),
                                             len(best['green']),
